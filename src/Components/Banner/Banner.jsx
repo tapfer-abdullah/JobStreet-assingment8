d4dfd395b849cc5bc969b3c476a3c1d98498ee88
tapfer-abdullah <tapfer.abdullah@gmail.com>
2023-04-10 14:04:48 +0600

    log img changed

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./Banner.css";
 import { Link } from 'react-router-dom';
-import IMG from "../../Images/nothing.jpg";
+import IMG from "../../assets/assets/All Images/P3OLGJ1 copy 1.png";
 
 const Banner = () => {
     return (
