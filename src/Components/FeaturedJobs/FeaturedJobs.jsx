5c1e87742b23497061d99162a29887a860951053
tapfer-abdullah <tapfer.abdullah@gmail.com>
2023-04-10 16:45:58 +0600

    see all jobs btn added

--- src/Components/FeaturedJobs/FeaturedJobs.jsx
+++ src/Components/FeaturedJobs/FeaturedJobs.jsx
@@ -1,10 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 import FJob from './FJob';
 
 const FeaturedJobs = () => {
-    // let fourJobs = [];
-    const [jobs, setJobs] = useState([]);
+
+    let isClicked = 0;
+    const seeAllJobs = () =>{
+        isClicked = isClicked+1;
+        console.log(isClicked)
+    }
+
+    let [jobs, setJobs] = useState([]);
 
     useEffect(() =>{
         fetch("FeaturedJobs.json")
@@ -12,21 +18,17 @@ const FeaturedJobs = () => {
         .then(data => setJobs(data));
     } ,[]);
 
-
+    if(!isClicked){
+        jobs = jobs.slice(0,4);
+        // seeMoreBtn(true);
+    }
     
-    // const seeAllJobs = () =>{
-    //     fourJobs = jobs;
-    //     console.log("c")
-    // }
 
-    
-    // for(let i=0; i<4; i++){
-    //     fourJobs.push(jobs[i]);
-    // }
-
-    // setJobs(jobs.slice(0, 3));
-
-    // console.log(fourJobs)
+    // useEffect( () =>{
+    //     fetch("FeaturedJobs.json")
+    //     .then(res => res.json())
+    //     .then(data => setJobs(data));
+    // } ,[])
     return (
         <div className='py-8 px-40 my-28 text-center'>
             <h3 className='text-3xl font-semibold mb-4'>Featured Jobs</h3>
@@ -36,8 +38,10 @@ const FeaturedJobs = () => {
                 {
                     jobs.map(job => <FJob key={job.id} job={job}></FJob>)
                 }
-                {/* <button onClick={seeAllJobs}>see more</button> */}
             </div>
+            <button className='mt-10 mb-32 py-2 px-3 bg-slate-500 rounded-md w-40 h-16 mx-auto' onClick={seeAllJobs}>
+                <Link className='text-white font-bold bg-slate-500'>See All Jobs</Link>
+            </button>
         </div>
     );
 };
